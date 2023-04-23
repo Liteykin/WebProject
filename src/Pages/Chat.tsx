7d457c55dabf3d0c9aea55ae3d9dd9cd1a6db428
Mark <liteykin.mark@gmail.com>
2023-04-23 16:21:43 +0200

    Resdesign

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -1,10 +1,7 @@
 import type { Component } from 'solid-js';
-import {lazy} from "solid-js";
-const NavChat= lazy(() => import('../Components/NavChat'));
 const Chat: Component = () => {
     return (
         <div>
-            <NavChat />
         </div>
     );
 };
