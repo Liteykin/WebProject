170f89fac783e4f35fdbcbd5cf9c89ef2cd7e40f
Mark <liteykin.mark@gmail.com>
2023-06-11 15:36:09 +0200

    Calendar add (unfisihed)

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,10 +1,11 @@
 import type { Component } from 'solid-js';
 import {lazy} from "solid-js";
+import Calendar from "../Components/Calendar";
 const Dashboard: Component = () => {
     return (
         <div class="m-0 p-0">
-            <div  class="snap-start bg-cyan-200 w-screen h-screen flex items-center justify-center text-8xl">
-                1
+            <div  class="snap-start bg-cyan-200 h-screen flex">
+                <Calendar />
             </div>
         </div>
     );
